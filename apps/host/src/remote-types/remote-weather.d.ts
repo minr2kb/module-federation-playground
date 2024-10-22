cc73a3847fa283337572ec5c6ed923671b1d3a6d
Kyungbae Min <kbmin1129@gmail.com>
2024-10-22 20:32:51 +0900

    type-build-util 패키지 추가

---
+++ apps/host/src/remote-types/remote-weather.d.ts
@@ -0,0 +1,30 @@
+// This d.ts file is automatically generated
+declare module 'remote-weather/WeatherWidget' {
+  export type WeatherWidgetProps = {
+  	onClick: () => void;
+  };
+  declare const _default: (props: WeatherWidgetProps & Partial<Pick<import("@tanstack/react-query").QueryClientProviderProps, "client">>) => import("react/jsx-runtime").JSX.Element;
+  
+  export {
+  	_default as default,
+  };
+  
+  export as namespace WeatherWidget;
+  
+  export {};
+  
+}
+
+declare module 'remote-weather/WeatherPage' {
+  declare const _default: (props: object & Partial<Pick<import("@tanstack/react-query").QueryClientProviderProps, "client">>) => import("react/jsx-runtime").JSX.Element;
+  
+  export {
+  	_default as default,
+  };
+  
+  export as namespace WeatherPage;
+  
+  export {};
+  
+}
+
--- apps/remote-weather/vite.config.ts
+++ apps/remote-weather/vite.config.ts
@@ -2,11 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import federation from '@originjs/vite-plugin-federation';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import mfConfigs from './mf.config.json';
+import exposes from './exposes.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), federation(mfConfigs)],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    federation({
+      name: 'remote-weather',
+      filename: 'remoteEntry.js',
+      exposes,
+      shared: [
+        'react',
+        'react-dom',
+        '@chakra-ui/react',
+        '@tanstack/react-query',
+      ],
+    }),
+  ],
   cacheDir: './.vite',
   build: {
     modulePreload: false,
